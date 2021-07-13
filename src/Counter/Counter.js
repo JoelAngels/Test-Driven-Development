2a58712cb414d92ba4b2ecd96a5f14af87b3eaf8
Joel Angel <joelbaraka4325@gmail.com>
2021-07-13 10:31:06 +0300

    added all my test driver deveopment tests

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  return <h1 data-testid="header">My Counter</h1>;
+  const [counterValue, setCounterValue] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+  return (
+    <>
+      <h1 data-testid="header">My Counter</h1>
+      <h2 data-testid="counter">{counterValue}</h2>
+      <button data-testid="subtract-btn">-</button>
+      <button data-testid="add-btn">+</button>
+      <input type="number" data-testid="input" value={inputValue} />
+    </>
+  );
 };
 
 export default Counter;
--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -9,3 +9,31 @@ test("header renders with correct text", () => {
 
   expect(headerElement.textContent).toBe("My Counter");
 });
+
+test("counter initially starts with test of 0", () => {
+  const { getByTestId } = render(<Counter />);
+  const counterElement = getByTestId("counter");
+
+  expect(counterElement.textContent).toBe("0");
+});
+
+test("initial value that starts with 1", () => {
+  const { getByTestId } = render(<Counter />);
+  const inputElement = getByTestId("input");
+
+  expect(inputElement.value).toBe("1");
+});
+
+test("add buttons renders with +", () => {
+  const { getByTestId } = render(<Counter />);
+  const addButton = getByTestId("add-btn");
+
+  expect(addButton.textContent).toBe("+");
+});
+
+test("subtract buttons renders with -", () => {
+  const { getByTestId } = render(<Counter />);
+  const subtractButton = getByTestId("subtract-btn");
+
+  expect(subtractButton.textContent).toBe("-");
+});
